edbccfda0b2e26422ce2e2a233c44b0881dfc117
sufyanadam <sufyan.adam@gmail.com>
2013-03-27 23:52:31 -0700

    new emacs prelude setup - the basics

---
+++ default-font.el
@@ -0,0 +1,14 @@
+(when (eq system-type 'darwin)
+  ;; These configuration seems to work in
+  ;; GNU Emacs 24.1.1 (x86_64-apple-darwin, NS apple-appkit-1038.36)
+  ;; of 2012-06-11 on bob.porkrind.org
+
+  ;; default font family
+  (set-face-attribute 'default nil :family "Consolas")
+
+  ;; default font size
+  ;;
+  ;; WARNING: depending on the default font, some height value may
+  ;; cause a broken frame display; that is, the beginning of the
+  ;; buffer is not visible.
+  (set-face-attribute 'default nil :height 165))
---
+++ w3m.el
@@ -0,0 +1,5 @@
+;; Enable tabs in w3m
+(setq w3m-use-tab t)
+
+;; Enable cookies in w3m
+(setq w3m-use-cookies t)
